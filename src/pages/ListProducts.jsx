82cee576ef3ea36b562f9072cf528e18a92552a1
Lorena Moreira Garcia <garcia.lmoreira@gmail.com>
2022-09-08 18:03:56 -0300

    requisito 4

--- src/pages/ListProducts.jsx
+++ src/pages/ListProducts.jsx
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
 
 export default class ListProducts extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    this.handleChange();
+  }
+
+  handleChange = async () => {
+    const listOfProducts = await getCategories();
+    console.log(listOfProducts);
+    this.setState({
+      data: listOfProducts,
+    });
+  };
+
   render() {
+    const { data } = this.state;
     return (
       <section>
         <input type="text" value="" />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <div>
+          <ul>
+            {data.map((list) => (
+              <li key={ list.id }>
+                <button
+                  data-testid="category"
+                  type="button"
+                  id="cartegories"
+                  onChange={ this.handleChange }
+                >
+                  { list.name }
+                </button>
+              </li>
+            ))}
+            ;
+          </ul>
+        </div>
       </section>
     );
   }
