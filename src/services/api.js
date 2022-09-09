6b6ecc002969cfc69484de07d98f83deda0a741b
ronerog <ronerojr@gmail.com>
2022-09-09 19:55:20 -0300

    req 8 em construção

--- src/services/api.js
+++ src/services/api.js
@@ -8,7 +8,6 @@ export async function getCategories() {
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
   let url = '';
   if (!categoryId) {
     url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
