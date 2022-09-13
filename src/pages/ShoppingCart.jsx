d2ba579337dc7143c0b9d2dadbe77b8881bf8685
Lorena Moreira Garcia <garcia.lmoreira@gmail.com>
2022-09-12 21:52:11 -0300

    requisito 10 - botão remover ok

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 class ShoppingCart extends React.Component {
   state = {
     stateCart: [],
+    quantidades: {},
   };
 
   componentDidMount() {
@@ -21,47 +22,50 @@ class ShoppingCart extends React.Component {
   //   this.setState({ stateCart: test1 });
   // };
 
-  // handleIncrease = (mais) => {
-  //   const item = JSON.parse(localStorage.getItem('cart')) || [];
-  //   const test = item.filter((qt) => qt.id === mais.id).length;
-  //   this.setState({ stateCart: test });
-  // };
+  handleIncrease = (mais) => {
+    const item = JSON.parse(localStorage.getItem('cart')) || [];
+    const quantidade = item.filter((qt) => qt.id === mais.id).length;
+    const soma = quantidade + 1;
+    const { id } = mais;
+    this.setState(({ quantidades: prevQuantidades }) => ({
+      quantidades: { [id]: soma, ...prevQuantidades },
+    }));
+  };
 
-  // handleRemove = () => {
-  //   localStorage.removeItem('cart');
-  // };
+  handleRemove = (obj) => {
+    const item = JSON.parse(localStorage.getItem('cart')) || [];
+    const filter = item.filter((e) => e.id !== obj.id);
+    localStorage.setItem('cart', JSON.stringify(filter));
+    this.handleLocalStorage();
+  };
 
   render() {
-    const { stateCart } = this.state;
+    const { stateCart, quantidades } = this.state;
 
     return (
       <section>
         <Link to="/"> Página Inicial </Link>
 
         <h1>Carrinho de Compras</h1>
-        { (stateCart.length > 0)
-          ? (stateCart.map((item) => (
+        {stateCart.length > 0 ? (
+          stateCart.map((item) => (
             <div key={ item.id }>
-              <p data-testid="shopping-cart-product-name">{ item.title }</p>
-              <p>{ item.price }</p>
+              <p data-testid="shopping-cart-product-name">{item.title}</p>
+              <p>{item.price}</p>
               <img src={ item.thumbnail } alt={ item.title } />
               <button
                 type="button"
                 // onClick={ () => this.handleDecrease(item.id) }
-                data-testid="product-decrease-quantity">
+                data-testid="product-decrease-quantity"
+              >
                 -
               </button>
-              {stateCart.filter((qt) => qt.id === item.id) && (
-                <button
-                  data-testid="shopping-cart-product-quantity"
-                  type="button"
-                >
-                  {stateCart.filter((qt) => qt.id === item.id).length}
-
-                </button>)}
+              <p data-testid="shopping-cart-product-quantity">
+                {quantidades[item.id]}
+              </p>
               <button
                 type="button"
-                // onClick={ () => this.handleIncrease(item.id) }
+                onClick={ () => this.handleIncrease(item) }
                 data-testid="product-increase-quantity"
               >
                 +
@@ -69,20 +73,23 @@ class ShoppingCart extends React.Component {
               <div>
                 <button
                   type="button"
-                  // onClick={ () => this.handleRemove(item.id) }
+                  onClick={ () => this.handleRemove(item) }
                   data-testid="remove-product"
                 >
                   Remove
                 </button>
               </div>
-            </div>)))
-          : (<p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>)}
-
+            </div>
+          ))
+        ) : (
+          <p data-testid="shopping-cart-empty-message">
+            Seu carrinho está vazio
+          </p>
+        )}
       </section>
     );
   }
 }
 
-ShoppingCart.propTypes = {
-};
+ShoppingCart.propTypes = {};
 export default ShoppingCart;
