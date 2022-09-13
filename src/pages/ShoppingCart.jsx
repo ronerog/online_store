df10a4137190f77a2a90bb26431607edf01642a9
Lorena Moreira Garcia <garcia.lmoreira@gmail.com>
2022-09-12 21:01:09 -0300

    iniciando requisito 10

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -15,6 +15,22 @@ class ShoppingCart extends React.Component {
     this.setState({ stateCart: item });
   };
 
+  // handleDecrease = (menos) => {
+  //   const item = JSON.parse(localStorage.getItem('cart')) || [];
+  //   const test1 = item.filter((qt) => qt.id !== menos.id).length;
+  //   this.setState({ stateCart: test1 });
+  // };
+
+  // handleIncrease = (mais) => {
+  //   const item = JSON.parse(localStorage.getItem('cart')) || [];
+  //   const test = item.filter((qt) => qt.id === mais.id).length;
+  //   this.setState({ stateCart: test });
+  // };
+
+  // handleRemove = () => {
+  //   localStorage.removeItem('cart');
+  // };
+
   render() {
     const { stateCart } = this.state;
 
@@ -29,6 +45,12 @@ class ShoppingCart extends React.Component {
               <p data-testid="shopping-cart-product-name">{ item.title }</p>
               <p>{ item.price }</p>
               <img src={ item.thumbnail } alt={ item.title } />
+              <button
+                type="button"
+                // onClick={ () => this.handleDecrease(item.id) }
+                data-testid="product-decrease-quantity">
+                -
+              </button>
               {stateCart.filter((qt) => qt.id === item.id) && (
                 <button
                   data-testid="shopping-cart-product-quantity"
@@ -37,6 +59,22 @@ class ShoppingCart extends React.Component {
                   {stateCart.filter((qt) => qt.id === item.id).length}
 
                 </button>)}
+              <button
+                type="button"
+                // onClick={ () => this.handleIncrease(item.id) }
+                data-testid="product-increase-quantity"
+              >
+                +
+              </button>
+              <div>
+                <button
+                  type="button"
+                  // onClick={ () => this.handleRemove(item.id) }
+                  data-testid="remove-product"
+                >
+                  Remove
+                </button>
+              </div>
             </div>)))
           : (<p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>)}
 
